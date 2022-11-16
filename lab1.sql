48016befd164b5814b55faffa95b48be5cf71c34
kowal <rogalix1308@gmail.com>
2022-11-16 22:07:35 +0100

    added functions

--- lab1.sql
+++ lab1.sql
@@ -72,3 +72,68 @@ CREATE OR REPLACE VIEW V_Trips as
 CREATE OR REPLACE VIEW V_AvailableTrips as
     select T.COUNTRY, T.TRIP_DATE, T.NAME, T.NO_PLACES, available_places from V_TRIPS T
     where T.AVAILABLE_PLACES > 0;
+
+create or replace type reservation_type FORCE as object
+    (
+        COUNTRY varchar2(50),
+        TRIP_DATE DATE,
+        NAME varchar2(100),
+        FIRSTNAME varchar2(50),
+        LASTNAME varchar2(50),
+        RESERVATION_ID number,
+        STATUS char
+    );
+CREATE TYPE Trip_Participants_type1 as table of reservation_type;
+
+CREATE OR REPLACE FUNCTION TripParticipants (tripID IN NUMBER)
+    return Trip_Participants_type1
+IS
+    tab Trip_Participants_type1;
+    trips number;
+BEGIN
+    select count(*) into trips from TRIP where TRIP_ID=tripID;
+
+    if  trips=0 then
+        raise_application_error(-20101,'trip_id does not exist!');
+    end if;
+    SELECT  reservation_type(COUNTRY, TRIP_DATE, NAME, FIRSTNAME, LASTNAME, R.RESERVATION_ID, R.STATUS) BULK COLLECT
+    into tab from V_RESERVATIONS R
+        join RESERVATION R2 on R.RESERVATION_ID = R2.RESERVATION_ID
+        where R2.TRIP_ID=tripID;
+    return tab;
+end;
+
+create or replace function PersonReservations(personID in NUMBER)
+return Trip_Participants_type1
+as
+    persons number;
+    tab Trip_Participants_type1;
+BEGIN
+    select count(*) into persons from PERSON where PERSON_ID = personID;
+    if persons = 0 then
+        raise_application_error(-20102,'person_id does not exist!');
+    end if;
+    SELECT  reservation_type(COUNTRY, TRIP_DATE, NAME, P.FIRSTNAME, P.LASTNAME, R.RESERVATION_ID, R.STATUS) BULK COLLECT
+    into tab from V_RESERVATIONS R
+        join RESERVATION R2 on R.RESERVATION_ID = R2.RESERVATION_ID
+        join PERSON P on P.PERSON_ID = R2.PERSON_ID
+        where P.PERSON_ID = personID;
+    return tab;
+END;
+
+create or replace type available_trips as object
+(
+    COUNTRY varchar2(50),
+    TRIP_DATE DATE
+);
+create or replace type available_trips_type as TABLE OF available_trips;
+
+Create or replace function AvailableTrips(countryName IN varchar2, date_from IN Date, date_to IN DATE)
+return available_trips_type
+as
+    tab available_trips_type;
+BEGIN
+    select available_trips(T.COUNTRY, T.TRIP_DATE) bulk collect into tab from TRIP T
+    where T.COUNTRY = countryName and t.TRIP_DATE BETWEEN date_from and date_to;
+    return tab;
+end;
